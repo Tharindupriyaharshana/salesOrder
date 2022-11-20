9df732374e400c12ee45f953afae7ce8990dcdc2
TharinduPriya <ewebstharindu@gmail.com>
2022-11-20 11:46:12 -0800

    create Rest Api Layout

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+//Import routers
+const V1orderRouter=require("./V1/routes/orderRouters");
+
+
+  
+
+
+
+const PORT = process.env.PORT || 3000; 
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+});
+
+
+
+app.use("/api/v1/orders", V1orderRouter);
+
+
+app.listen(PORT, () => { 
+    console.log(`API is listening on port ${PORT}`); 
+});
