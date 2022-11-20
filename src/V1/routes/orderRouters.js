9ab7d35649a1855f907cc87d679f4444ad8d2a95
TharinduPriya <ewebstharindu@gmail.com>
2022-11-20 12:22:08 -0800

    Service Layer addming and minner chnagers

--- src/V1/routes/orderRouters.js
+++ src/V1/routes/orderRouters.js
@@ -1,25 +1,17 @@
 // In src/v1/routes/workoutRoutes.js
 const express = require("express");
+const orderController = require("../../controllers/orderController");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Get all orders");
-});
+router.get("/", orderController.getAllorders);
 
-router.get("/:orderId", (req, res) => {
-  res.send("Get an existing order");
-});
+router.get("/:orderId",orderController.getOneOrder);
 
-router.post("/", (req, res) => {
-  res.send("Create a new order");
-});
+router.post("/", orderController.createNewOrder);
 
-router.patch("/:workoutId", (req, res) => {
-  res.send("Update an existing order");
-});
+router.patch("/:orderId",orderController.updateOneOrder);
 
-router.delete("/:workoutId", (req, res) => {
-  res.send("Delete an existing order");
-});
+router.delete("/:orderId",orderController.deleteOneOrder);
 
 module.exports = router;
---
+++ src/service/orderService.js
@@ -0,0 +1,27 @@
+const getAllOrders = () => {
+    return;
+  };
+  
+  const getOneOrder = () => {
+    return;
+  };
+  
+  const createNewOrder = () => {
+    return;
+  };
+  
+  const updateOneOrder = () => {
+    return;
+  };
+  
+  const deleteOneOrder = () => {
+    return;
+  };
+  
+  module.exports = {
+    getAllOrders,
+    getOneOrder,
+    createNewOrder,
+    updateOneOrder,
+    deleteOneOrder,
+  };
